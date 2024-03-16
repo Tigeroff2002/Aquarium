94ce5dcd27741e33e854ce89c3b33b33169b91b3
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-03-16 13:52:46 +0300

    add texture loading logic to separate class

--- Form1.cs
+++ Form1.cs
@@ -334,6 +334,7 @@ namespace Aquarium
         #endregion
 
         #region fractal drawing
+
         // функция отрисовки
         private void DrawFractal()
         {
@@ -461,90 +462,15 @@ namespace Aquarium
         #region texture loading
         private void выбратьТекстуруToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            var textureLoader = new TextureLoader();
+
             DialogResult res = openFileDialog1.ShowDialog(); // если файл выбран и возвращен результат - OK
 
             if (res == DialogResult.OK)
             {
-                // создаем изображение с идентификатором imageId
-                Il.ilGenImages(1, out imageId);
-                // делаем изображение текущим
-                Il.ilBindImage(imageId);
-
-                // адрес изображения, полученный с помощью окна выбра файла
-                string url = openFileDialog1.FileName;
-
-                // пробуем загрузить изображение
-                if (Il.ilLoadImage(url))
-                {
-                    // если загрузка прошла успешно
-                    // сохраняем размеры изображения
-                    int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
-                    int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
-
-                    // определяем число бит на пиксель
-                    int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
-
-                    switch (bitspp) // в зависимости от полученного результата
-                    {
-
-                        // создаем текстуру, используя режим GL_RGB или GL_RGBA
-                        case 24:
-                            mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
-                            break;
-                        case 32:
-                            mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
-                            break;
-
-                    }
-
-                    // активируем флаг, сигнализирующий загрузку текстуры
-                    textureIsLoad = true;
-                    // очищаем память
-                    Il.ilDeleteImages(1, ref imageId);
-
-                }
+                mGlTextureObject = textureLoader.LoadTexture(openFileDialog1.FileName);
+                textureIsLoad = true;
             }
-        }
-
-        // создание текстуры в памяти openGL
-        private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
-        {
-            // идентификатор текстурного объекта
-            uint texObject;
-
-            // генерируем текстурный объект
-            Gl.glGenTextures(1, out texObject);
-
-            // устанавливаем режим упаковки пикселей
-            Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
-
-            // создаем привязку к только что созданной текстуре
-            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
-
-            // устанавливаем режим фильтрации и повторения текстуры
-            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
-            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
-            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
-            Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
-            Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
-
-            // создаем RGB или RGBA текстуру
-            switch (Format)
-            {
-
-                case Gl.GL_RGB:
-                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
-                    break;
-
-                case Gl.GL_RGBA:
-                    Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
-                    break;
-
-            }
-
-            // возвращаем идентификатор текстурного объекта
-
-            return texObject;
         }
         #endregion
 
--- TextureLoader.cs
+++ TextureLoader.cs
@@ -1,95 +1,88 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System;
 using Tao.OpenGl;
 using Tao.DevIl;
+using System.Windows.Forms;
 
 namespace Aquarium
 {
     // not working
     public sealed class TextureLoader
     {
-        private string texture_name = "";
         private int imageId = 0;
         private uint mGlTextureObject = 0;
+        private bool textureIsLoad;
 
-        Dictionary<string, string> textures = new Dictionary<string, string>
+        public uint LoadTexture(string fileName)
         {
-            {"background", "..\\..\\texture\\background.jpg"},
-            {"aquarium", "..\\..\\texture\\aquarium.jpg"},
-            {"fish", "texture\\fish.jpg"},
-            {"sand", "texture\\sand.jpg"},
-            {"vodorosli", "texture\\vodorosli.jpg"},
-            {"mollusk", "texture\\mollusk.jpg"},
-        };
-
-        public uint GetTextureObj()
-        {
-            return mGlTextureObject;
-        }
-
-        public void LoadTextureForModel(string FileName)
-        {
-            texture_name = FileName;
-
+            // создаем изображение с идентификатором imageId
             Il.ilGenImages(1, out imageId);
+            // делаем изображение текущим
             Il.ilBindImage(imageId);
 
-            string fileName = "";
-
-            foreach (var texture in textures)
+            // пробуем загрузить изображение
+            if (Il.ilLoadImage(fileName))
             {
-                if (texture_name.Contains(texture.Key))
-                    fileName = texture.Value;
-            }
+                // если загрузка прошла успешно
+                // сохраняем размеры изображения
+                int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
+                int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
 
-            Il.ilDisable(Il.IL_ORIGIN_SET);
-            Il.ilEnable(Il.IL_CONV_PAL);
+                // определяем число бит на пиксель
+                int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
 
-            string url = Path.GetFullPath(fileName);
-
-            var existense = File.Exists(url);
-
-            if (existense)
-            {
-                if (Il.ilLoadImage(url))
+                switch (bitspp) // в зависимости от полученного результата
                 {
-                    int width = Il.ilGetInteger(Il.IL_IMAGE_WIDTH);
-                    int height = Il.ilGetInteger(Il.IL_IMAGE_HEIGHT);
 
-                    int bitspp = Il.ilGetInteger(Il.IL_IMAGE_BITS_PER_PIXEL);
+                    // создаем текстуру, используя режим GL_RGB или GL_RGBA
+                    case 24:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
+                        break;
+                    case 32:
+                        mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
+                        break;
 
-                    switch (bitspp)
-                    {
-                        case 24:
-                            mGlTextureObject = MakeGlTexture(Gl.GL_RGB, Il.ilGetData(), width, height);
-                            break;
-                        case 32:
-                            mGlTextureObject = MakeGlTexture(Gl.GL_RGBA, Il.ilGetData(), width, height);
-                            break;
-                    }
-
-                    Il.ilDeleteImages(1, ref imageId);
                 }
+
+                // активируем флаг, сигнализирующий загрузку текстуры
+                textureIsLoad = true;
+
+                // очищаем память
+                Il.ilDeleteImages(1, ref imageId);
+
+                return mGlTextureObject;
             }
+
+            throw new InvalidOperationException("No such picture this choosen file name");
         }
 
+        // создание текстуры в памяти openGL
         private static uint MakeGlTexture(int Format, IntPtr pixels, int w, int h)
         {
+            // идентификатор текстурного объекта
             uint texObject;
 
+            // генерируем текстурный объект
             Gl.glGenTextures(1, out texObject);
+
+            // устанавливаем режим упаковки пикселей
             Gl.glPixelStorei(Gl.GL_UNPACK_ALIGNMENT, 1);
+
+            // создаем привязку к только что созданной текстуре
             Gl.glBindTexture(Gl.GL_TEXTURE_2D, texObject);
 
+            // устанавливаем режим фильтрации и повторения текстуры
             Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_S, Gl.GL_REPEAT);
             Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_WRAP_T, Gl.GL_REPEAT);
             Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_LINEAR);
             Gl.glTexParameteri(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR);
             Gl.glTexEnvf(Gl.GL_TEXTURE_ENV, Gl.GL_TEXTURE_ENV_MODE, Gl.GL_REPLACE);
 
+            // создаем RGB или RGBA текстуру
             switch (Format)
             {
+
                 case Gl.GL_RGB:
                     Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGB, w, h, 0, Gl.GL_RGB, Gl.GL_UNSIGNED_BYTE, pixels);
                     break;
@@ -97,13 +90,12 @@ namespace Aquarium
                 case Gl.GL_RGBA:
                     Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, Gl.GL_RGBA, w, h, 0, Gl.GL_RGBA, Gl.GL_UNSIGNED_BYTE, pixels);
                     break;
-            }
-            return texObject;
-        }
 
-        public void Method()
-        {
-            throw new System.NotImplementedException();
+            }
+
+            // возвращаем идентификатор текстурного объекта
+
+            return texObject;
         }
     }
 }
